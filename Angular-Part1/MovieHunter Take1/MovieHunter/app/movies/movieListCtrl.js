ebfc5462f0adb1d34293b35f2fde93f963996595
DeborahK <deborahk@insteptech.com>
2015-04-16 17:50:38 -0700

    Added code for the end of Part 1.

---
+++ Angular-Part1/MovieHunter Take1/MovieHunter/app/movies/movieListCtrl.js
@@ -0,0 +1,44 @@
+﻿(function () {
+    "use strict";
+
+    angular
+    .module("movieHunter")
+    .controller("MovieListCtrl",
+                [MovieListCtrl]);
+
+    function MovieListCtrl() {
+        var vm = this;
+
+        vm.movies = [
+            {
+                title: 'The Fellowship of the Ring',
+                director: 'Jackson',
+                date: '2001-12-19',
+                imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Fellowship-Of-The-Ring_small.jpg',
+                mpaa: 'pg-13'
+            },
+             {
+                 title: 'The Two Towers',
+                 director: 'Jackson',
+                 date: '2002-12-18',
+                 imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Two-Towers_small.jpg',
+                 mpaa: 'pg-13'
+             },
+		     {
+		         title: 'The Point',
+		         director: 'Wolf',
+		         date: '1971-02-02',
+		         imageurl: '',
+		         mpaa: 'nr'
+		     }
+        ];
+        vm.title = "Search by Movie Title";
+        vm.showImage = false;
+
+        vm.toggleImage = function () {
+            vm.showImage = !vm.showImage;
+        };
+    }
+
+}());
+
