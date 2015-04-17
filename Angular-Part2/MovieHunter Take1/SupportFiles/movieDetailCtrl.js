fa8116d1d349a3b7fc9aeabc334c0bb3db10b391
DeborahK <deborahk@insteptech.com>
2015-04-17 10:47:26 -0700

    Added the Take 2 code.

---
+++ Angular-Part2/MovieHunter Take1/SupportFiles/movieDetailCtrl.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("movieHunter")
+        .controller("MovieDetailCtrl",
+                    ["$routeParams",
+                     MovieDetailCtrl]);
+
+    function MovieDetailCtrl($routeParams) {
+        var vm = this;
+
+        vm.movieId = $routeParams.movieId;
+        vm.movie = {
+            movieId: 1,
+            description: 'A meek hobbit of the Shire and eight companions set out on a journey to Mount Doom to destroy the One Ring and the dark lord Sauron.',
+            title: 'The Fellowship of the Ring',
+            director: 'Jackson',
+            releaseDate: '2001-12-19',
+            imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Fellowship-Of-The-Ring_small.jpg',
+            mpaa: 'pg-13'
+        }
+
+    }
+}());
--- Angular-Part2/readme.md
+++ Angular-Part2/readme.md
@@ -1,1 +1,5 @@
 Materials for the "Building An Angular Web App from the Ground Up - Part 2" session on the Software Design and Developer conference in London 2015.
+
+Take 1: The starting code for this session. Includes the clipboard text to build the routing code.
+Take 2: The completed routing code. Includes the clipboard text to build the data access code.
+Take 3: The completed code.
